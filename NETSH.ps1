dee625d51c961cddc94fa5f5d387eb3203f9bb06
Donovin DeCoste <Donovin.DeCoste@quest.com>
2017-12-20 13:11:33 -0700

    test1

--- NETSH.ps1
+++ NETSH.ps1
@@ -1,5 +1,8 @@
-﻿$pw = ConvertTo-SecureString template!PWD -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ('CAAdmin', $pw)
+﻿$pw = ConvertTo-SecureString "template!PWD" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\caadmin", $pw)
+Enable-PSRemoting –force
 $session = New-PSSession  -Credential $cred
 Invoke-Command -Session $session -ScriptBlock {Netsh trace start scenario=netconnection capture=yes report=yes persistent=yes maxsize=4096 tracefile=c:\trace.etly}
+
+
 
