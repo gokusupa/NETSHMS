fac73132b03b97cd40926810f6e81a08f1d55e1f
Donovin DeCoste <Donovin.DeCoste@quest.com>
2017-12-20 13:20:27 -0700

    commit2

---
+++ NETSHStart.ps1
@@ -0,0 +1,5 @@
+﻿$password =  ConvertTo-SecureString "template!PWD" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\caadmin", $password)
+$command = $file = $PSScriptRoot + "\NETSH2.ps1"
+Enable-PSRemoting –force
+Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME
