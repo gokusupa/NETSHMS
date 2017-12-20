14f33a103a7fdfdaef6f993423d55905e9cde025
Donovin DeCoste <Donovin.DeCoste@quest.com>
2017-12-20 12:48:33 -0700

    add more

--- NETSH.ps1
+++ NETSH.ps1
@@ -1,2 +1,5 @@
-﻿Netsh trace start scenario=netconnection capture=yes report=yes persistent=yes maxsize=4096 tracefile=c:\trace.etl
-break
+﻿$pw = ConvertTo-SecureString template!PWD -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ('CAAdmin', $pw)
+$session = New-PSSession  -Credential $cred
+Invoke-Command -Session $session -ScriptBlock {Netsh trace start scenario=netconnection capture=yes report=yes persistent=yes maxsize=4096 tracefile=c:\trace.etly}
+
